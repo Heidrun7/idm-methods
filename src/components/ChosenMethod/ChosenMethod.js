315c8854127cb51207db6436243ff066546b1bbb
Heidrun Heidarsdottir <heidrunh17@ru.is>
2021-10-10 00:53:50 +0200

    Added chosen method page

--- src/components/ChosenMethod/ChosenMethod.js
+++ src/components/ChosenMethod/ChosenMethod.js
@@ -1,22 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import "./ChosenMethod.css";
+import myMethodsList from "../../files/myMethods.json";
+import "./ChosenMethod.css";
 import { Typography, Divider } from "antd";
 
 const ChosenMethod = (props) => {
   const { Title, Paragraph, Text, Link } = Typography;
   const { id } = useParams();
 
+  let method = 0;
+
+  console.log("mymehtods: ", myMethodsList);
+  for (var i = 0; i < myMethodsList.length; i++) {
+    if (myMethodsList[i].Number == id) {
+      method = myMethodsList[i];
+      break;
+    }
+  }
+  console.log("Chosen method: ", method.Name);
+
+  let references = <></>//method.References.split("\n");
+
+  if (method.References.length) {
+    references = method.References.split(";").map((data, index) => {
+      console.log("data,index: ", data, index);
+      return <Paragraph>{data}</Paragraph>;
+    });
+  }
+
+  let tags = <></>//method.References.split("\n");
+  console.log("TAGS: ", method.Tags.split(";"))
+
+  if (method.Tags) {
+    tags = method.Tags.split(";").map((data, index) => {
+      return <Link className="tags">{data}</Link>;
+    });
+  }
+
   return (
     <div className="wrapper">
       <Typography>
-        <Title>Introduction {id}</Title>
-        <Paragraph>
-          In the process of internal desktop applications development, many
-          different design specs and implementations would be involved, which
-          might cause designers and developers difficulties and duplication and
-          reduce the efficiency of development.
-        </Paragraph>
+        <Title>
+          Method {id}: {method.Name}
+        </Title>
+        <Title level={2}>Input</Title>
+        <Paragraph>{method.Input}</Paragraph>
+        <Title level={2}>Output</Title>
+        <Paragraph>{method.Output}</Paragraph>
+        <Title level={2}>What it does</Title>
+        <Paragraph>{method.["What it does"]}</Paragraph>
+        <Title level={2}>How it works</Title>
+        <Paragraph>{method.["How it works"]}</Paragraph>
+        <Title level={2}>Experience</Title>
+        <Paragraph>{method.Experience}</Paragraph>
+        <Title level={2}>References</Title>
+        <Paragraph>{references}</Paragraph>
+        <Title level={2}>Tags</Title>
+        <Paragraph>{tags}</Paragraph>
       </Typography>
     </div>
   );
